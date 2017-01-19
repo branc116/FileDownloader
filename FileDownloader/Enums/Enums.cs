593b8aaa55701889097be3326a37e15d94046327
Branimir Ricko <rickobranimir@gmail.com>
2017-01-19 18:26:10 +0100

    Enums

---
+++ FileDownloader/Enums/Enums.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileDownloader.Enums
+{
+    public enum FileStatus
+    {
+        Nothing,
+        HasUri,
+        Downloading,
+        InMemory,
+        OnDisc,
+        OnDiscInMemory
+    }
+    public enum DownloadStatus
+    {
+        Ok,
+        BadUri,
+        TimeOut,
+        Error,
+    }
+    public enum DiscWriteStatus
+    {
+        Ok,
+        BadPath,
+        TimeOut,
+        Error
+    }
+}
