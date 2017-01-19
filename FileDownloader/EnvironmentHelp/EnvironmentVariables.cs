bd09686b249a8c59b0233f86a2f016f9d79b966d
Branimir Ricko <rickobranimir@gmail.com>
2017-01-19 18:25:51 +0100

    Helper Vars

---
+++ FileDownloader/EnvironmentHelp/EnvironmentVariables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileDownloader.EnvironmentHelp
+{
+    public static class EnvironmentVariables
+    {
+        public static string Home
+        {
+            get
+            {
+                return (Environment.GetEnvironmentVariable("USERPROFILE") ?? Environment.GetEnvironmentVariable("HOME") ?? "DUNNO").ToString();
+                
+            }
+        }
+        public static string DownloadFolder
+        {
+            get
+            {
+                return Path.Combine(Home, "Downloads");
+            }
+        }
+    }
+}
