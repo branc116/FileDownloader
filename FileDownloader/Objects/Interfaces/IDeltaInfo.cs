cf6fc11d9fbb87e5915fc1e62b787cc224fc7a59
Branimir Ricko <rickobranimir@gmail.com>
2017-01-19 18:24:56 +0100

    Interfaces

---
+++ FileDownloader/Objects/Interfaces/IDeltaInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileDownloader.Objects.Interfaces
+{
+    public interface IDeltaSizeInfo
+    {
+        DateTime StartedAt { get; set; }
+        DateTime EndedAt { get; set; }
+        TimeSpan Duration { get; }
+        string SizeAuto { get; }
+        long SizeBytes { get; set; }
+        int SizeKBytes { get; }
+        int SizeMBytes { get; }
+        int SizeGBytes { get; }
+        string CurrentSizeAuto { get; }
+        long CurrentSizeBytes { get; set; }
+        int CurrentSizeKBytes { get; }
+        int CurrentSizeMBytes { get; }
+        int CurrentSizeGBytes { get; }
+        string SpeedAuto { get; }
+        float SpeedMBps { get; }
+        float SpeedKBps { get; }
+        int SpeedBps { get; }
+
+    }
+}
---
+++ FileDownloader/Objects/Interfaces/IFileObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using FileDownloader.Args;
+using FileDownloader.Objects.Helper;
+using FileDownloader.Enums;
+
+namespace FileDownloader.Objects.Interfaces
+{
+    public interface IFileObject : IDisposable, IComparable
+    {
+        event EventHandler<UriChangedArgs> UriChange;
+        event EventHandler<FileDownloadedArgs> FileDownloaded;
+        event EventHandler<FileDownloadingArgs> FileDownloading;
+        event EventHandler<FileRemovedArgs> FileRemoved;
+        event EventHandler<FileSavingArgs> FileSaving;
+        event EventHandler<FileSavedArgs> FileSaved;
+        byte[] RawFile { get; }
+        UriObject Uri { get; set; }
+        string Name { get; set; }
+        FileStatus Status { get;}
+        object Tag { get; set; }
+        IDeltaSizeInfo DownloadInfo { get;}
+        IDeltaSizeInfo SaveInfo { get; }
+        IList<string> PathsOnDisc { get; }
+        void DeleteFromMemory();
+        Task<DownloadStatus> DownloadAsync();
+        Task<DiscWriteStatus> SaveOnDiscAsync(string Path);
+        Task<DiscWriteStatus> SaveOnDiscAsync();
+        Task<bool> RemoveFormDiscAsync();
+    }
+}
